6b852b481eecc1633f46454d769dacff303d4244
NIKITA LIANG <nikitaliang@NIKITAs-MBP.attlocal.net>
2024-07-22 00:32:53 -0700

    a6

--- App.js
+++ App.js
@@ -5,14 +5,42 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb+srv://rushanliang:kanbasproject@kanbas.biocdox.mongodb.net/?retryWrites=true&w=majority&appName=Kanbas";
+mongoose.connect(CONNECTION_STRING);
+
+const app = express()
+app.use(cors({
+   credentials: true,
+   origin: process.env.NETLIFY_URL || "http://localhost:3000",
+ })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+app.use(express.json()); // do all your work after this line
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);

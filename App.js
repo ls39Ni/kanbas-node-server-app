48dd771b3156415d931acb5b5af9af4388112952
NIKITA LIANG <nikitaliang@NIKITAs-MBP.lan>
2024-07-29 12:23:19 -0700

    test

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
+import "dotenv/config";
+import session from "express-session";
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
@@ -6,13 +10,48 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
 
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING ||
+  "mongodb+srv://rushanliang:kanbasproject@kanbas.biocdox.mongodb.net/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+// app.use(cors({
+//   credentials: true,
+// origin: function(origin, callback){
+//   const allowedOrigins = [process.env.NETLIFY_URL, 'http://localhost:3000'];
+//   if (!origin || allowedOrigins.includes(origin)) {
+//     callback(null, true);
+//   } else {
+//     callback(new Error('Not allowed by CORS'));
+//   }
+// }
+// }));
+app.use(express.json()); // Do all work after this line
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
-
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);

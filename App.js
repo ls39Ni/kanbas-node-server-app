bec4cfa8df37a9c6efb0704c18a14ec54d31f6b4
NIKITA LIANG <nikitaliang@NIKITAs-MBP.lan>
2024-07-28 22:45:10 -0700

    fixed a bug

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-import express from "express";
-import Hello from "./Hello.js";
-import Lab5 from "./Lab5/index.js";
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-import cors from "cors";
-import mongoose from "mongoose";
 import "dotenv/config";
-import UserRoutes from "./Users/routes.js";
 import session from "express-session";
-import "dotenv/config";
+import express from 'express';
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import Hello from './Hello.js';
+import Lab5 from './Lab5/index.js';
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import cors from "cors"
 
-const CONNECTION_STRING =
-  process.env.MONGO_CONNECTION_STRING ||
-  "mongodb+srv://rushanliang:kanbasproject@kanbas.biocdox.mongodb.net/kanbas";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb+srv://rushanliang:<password>@kanbas.biocdox.mongodb.net/?retryWrites=true&w=majority&appName=Kanbas"
 mongoose.connect(CONNECTION_STRING);
-
-const app = express();
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
-  })
-);
-app.use(express.json());
+const app = express()
+app.use(cors({
+  credentials: true,
+  origin: function(origin, callback){
+    const allowedOrigins = [process.env.NETLIFY_URL, 'http://localhost:3000'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+app.use(express.json()); // Do all work after this line
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
@@ -37,14 +38,14 @@ if (process.env.NODE_ENV !== "development") {
     domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
-app.use(session(sessionOptions));
-UserRoutes(app);
-CourseRoutes(app);
-ModuleRoutes(app);
-AssignmentRoutes(app);
-Lab5(app);
-Hello(app);
+app.use(session(sessionOptions)
+);
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 4000}`);
-});
+UserRoutes(app);
+Hello(app)
+CourseRoutes(app)
+ModuleRoutes(app)
+AssignmentRoutes(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000)
+
